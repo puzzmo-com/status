9661a3aff1caad3ef2e07cda5fff4fead5ff4d10
orta <git@orta.io>
2023-11-10 09:39:17 +0000

    More

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
 
 const production = process.env.NODE_ENV === "production";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: production ? '/' : '',
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "status"
 }
 
 module.exports = nextConfig
